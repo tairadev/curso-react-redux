b9e92cc52399b204e4158e51bf0651413831cc70
Guilherme Taira <guilhermeht1906@gmail.com>
2022-07-04 22:40:14 -0300

    Componente com Propriedade

---
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,12 @@
+export default function ComParametro(props) {
+    const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação';
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <p>
+                <strong>{props.aluno}</strong> 
+                tem nota {props.nota} e foi {status}
+            </p>
+        </div>
+    )
+}
---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+
+ReactDOM.render(
+  <div>
+    <Primeiro />
+    <ComParametro titulo='Situação do Aluno' aluno='Pedro Silva' nota={9.2} />
+  </div>,
+  document.getElementById('root')
+);
