685471df68f0d408f149611d56d5493638373327
Guilherme Taira <guilhermeht1906@gmail.com>
2022-07-22 01:12:19 -0300

    Calculadora início

---
+++ calculadora/src/App.js
@@ -0,0 +1,12 @@
+import Calculator from './main/Calculator';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
