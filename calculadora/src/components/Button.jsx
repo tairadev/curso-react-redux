6b44920e57c78cf3b287b17f3626ddd4e2ba4f29
Guilherme Taira <guilhermeht1906@gmail.com>
2022-07-22 13:37:52 -0300

    Calculadora finalizada

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
-  return <button className='button'>{props.label}</button>;
+  let classes = 'button ';
+  classes += props.operation ? ' operation' : '';
+  classes += props.double ? ' double' : '';
+  classes += props.triple ? ' triple' : '';
+  return (
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={`button ${classes}`}>
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
